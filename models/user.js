66b13f5a7de86e8137d47c2a250328449bf01918
Your GitHub Username <your-email@example.com>
2024-09-18 19:30:25 +0530

    member

--- models/user.js
+++ models/user.js
@@ -31,6 +31,39 @@ module.exports = (sequelize, DataTypes) => {
         key: 'id'
       }
     },
+    //////////////////
+    superior_ado: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'Users',
+        key: 'id'
+      }
+    },
+    superior_md: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'Users',
+        key: 'id'
+      }
+    },
+    superior_sd: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'Users',
+        key: 'id'
+      }
+    },
+    superior_d: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'Users',
+        key: 'id'
+      }
+    },
     pincode: DataTypes.STRING,
     state: DataTypes.STRING,
     city: DataTypes.STRING,
@@ -52,99 +85,14 @@ module.exports = (sequelize, DataTypes) => {
   User.associate = (models) => {
     User.belongsTo(models.Role, { foreignKey: 'role_id' });
     User.belongsTo(models.User, { foreignKey: 'superior_id', as: 'Parent' });
+
+        // Self-referencing associations for superiors
+        // User.belongsTo(models.User, { foreignKey: 'superior_id', as: 'Parent' });
+        User.belongsTo(models.User, { foreignKey: 'superior_ado', as: 'AdoSuperior' });
+        User.belongsTo(models.User, { foreignKey: 'superior_md', as: 'MdSuperior' });
+        User.belongsTo(models.User, { foreignKey: 'superior_sd', as: 'SdSuperior' });
+        User.belongsTo(models.User, { foreignKey: 'superior_d', as: 'DSuperior' });
   };
 
   return User;
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// 'use strict';
-
-// module.exports = (sequelize, DataTypes) => {
-//   const User = sequelize.define('User', {
-//     username: {
-//       type: DataTypes.STRING,
-//       allowNull: false
-//     },
-//     password: {
-//       type: DataTypes.STRING,
-//       allowNull: false
-//     },
-//     email: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//       unique: true,
-//     },
-//     role_id: {
-//       type: DataTypes.INTEGER,
-//       references: {
-//         model: 'Roles',
-//         key: 'id'
-//       },
-//       allowNull: false
-//     },
-
-//     superior_id: {
-//       type: DataTypes.INTEGER,
-//       allowNull: function() {
-//         return this.role_name !== 'admin';
-//       },
-//       references: {
-//         model: 'Users',
-//         key: 'id'
-//       }
-//     },
-
-//     pincode: DataTypes.STRING,
-//     state: DataTypes.STRING,
-//     city: DataTypes.STRING,
-//     street_name: DataTypes.STRING,
-//     building_no_name: DataTypes.STRING,
-//     // full_name: DataTypes.STRING,
-//     full_name: {
-//       type: DataTypes.STRING,
-//       allowNull: true 
-//     },
-//     mobile_number: DataTypes.STRING,
-//     role_name: {
-//       type: DataTypes.ENUM('admin', 'user'),
-//       allowNull: false
-//     }
-//   }, {
-//     timestamps: true // Ensure timestamps are managed by Sequelize
-//   });
-
-//   User.associate = (models) => {
-//     User.belongsTo(models.Role, { foreignKey: 'role_id' });
-//     User.belongsTo(models.User, { foreignKey: 'superior_id', as: 'Parent' });
-//   };
-
-//   return User;
-// };
