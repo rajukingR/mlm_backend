f638505581110c9bae7e82a83c3217b01f907032
Your GitHub Username <your-email@example.com>
2024-09-28 18:40:32 +0530

    memeber-createion-api-testing

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,19 +2,22 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const userHierarchyController = require('../controllers/hierarchy_user_controllers/userHierarchyController');
-const { authMiddleware } = require('../middlewares/authMiddleware');
-const {authUserMiddleware} = require('../middlewares/authUserMiddleware');
+const { authMiddleware,isAdmin } = require('../middlewares/authMiddleware');
 
 // User routes
 router.post('/signup', userController.signUp);
-router.post('/signin', userController.signIn); 
+router.post('/signin', userController.signIn);
 //
 router.put('/update/:userId', authMiddleware, userController.updateUser);
 router.delete('/delete/:userId', authMiddleware, userController.deleteUser);
 //
-router.get('/all', authMiddleware, userController.getAllUsers);
+router.get('/all',authMiddleware, userController.getAllUsers);
+
+// AuthMiddleware is not work well fix it
+
+
 //
-router.get('/hierarchy', authUserMiddleware, userHierarchyController.getHierarchicalUsers);
+// router.get('/hierarchy', authUserMiddleware, userHierarchyController.getHierarchicalUsers);
 
 
 module.exports = router;
