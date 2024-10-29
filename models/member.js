2412dd3b7c88c11c26eab4b33d61cd1143737b6d
rajukingR <rajumulinti060@gmail.com>
2024-10-29 17:33:53 +0530

    Update Edit profile Request API

---
+++ models/member.js
@@ -0,0 +1,56 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const Member = sequelize.define('Member', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    mobile_number: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      },
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    street_name: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    pincode: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  }, {
+    tableName: 'users',
+    timestamps: true,
+  });
+
+  // Association with Role model
+  Member.associate = (models) => {
+    Member.belongsTo(models.Role, { foreignKey: 'role_id' });
+  };
+
+  return Member;
+};
