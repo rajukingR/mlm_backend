7701c3fe318d20d200e3c2566da82af096a41ee8
Your GitHub Username <your-email@example.com>
2024-11-02 11:30:06 +0530

    feedback-api bug completed

--- routes/feedbackRoutes.js
+++ routes/feedbackRoutes.js
@@ -6,6 +6,7 @@ const feedbackController = require('../controllers/customer_feedback_controller/
 // Route for creating feedback
 router.post('/create', feedbackController.createFeedback);
 router.get('/hierarchy/:higher_role_id', feedbackController.getFeedbackForHigherRole);
+router.get('/customer/:customer_id', feedbackController.getFeedbackForCustomer);
 
 
 module.exports = router;
