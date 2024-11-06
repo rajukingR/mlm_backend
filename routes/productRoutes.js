33a341a9897a7a07b34bce220df27f31cda1ceb2
rajukingR <rajumulinti060@gmail.com>
2024-11-06 15:45:05 +0530

    UPDATE raju_server

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,14 +1,13 @@
-'use strict';
-
 const express = require('express');
-const productController = require('../controllers/productController');
 const router = express.Router();
+const productController = require('../controllers/productController');
+const upload = require('../middlewares/multer'); // Make sure Multer is set up to handle file uploads
 
-// Define routes for CRUD operations
-router.post('/', productController.createProduct);
-router.get('/', productController.getAllProducts);
-router.get('/:id', productController.getProductById);
-router.put('/:id', productController.updateProduct);
-router.delete('/:id', productController.deleteProduct);
+// Define routes for product operations
+router.post('/', upload.single('image'), productController.createProduct); // Route for creating a new product
+router.get('/', productController.getAllProducts); // Route to get all products
+router.get('/:id', productController.getProductById); // Route to get a single product by ID
+router.put('/:id', upload.single('image'), productController.updateProduct); // Route to update a product
+router.delete('/:id', productController.deleteProduct); // Route to delete a product
 
 module.exports = router;
