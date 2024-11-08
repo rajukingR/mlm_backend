261fa215fef9c11f22786c3dfb096095374a661f
Your GitHub Username <your-email@example.com>
2024-11-08 12:50:56 +0530

    achivement and target api completed

--- routes/userSalesDetailRoutes.js
+++ routes/userSalesDetailRoutes.js
@@ -1,16 +1,21 @@
+// In routes file
 const express = require('express');
 const router = express.Router();
 const userSalesDetail = require('../controllers/user_sales_detail/userSalesDetail');
 
-
-
-router.get('/role/:role', userSalesDetail.getSalesTargetByRole);
-router.get('/achivement/:user_id', userSalesDetail.calculateAchievement);
-
-
-
-
-
-
+router.get('/sales_achievement/:role/:user_id', userSalesDetail.getSalesTargetAndAchievement);
 
 module.exports = router;
+
+
+
+
+// const express = require('express');
+// const router = express.Router();
+// const userSalesDetail = require('../controllers/user_sales_detail/userSalesDetail');
+
+
+// router.get('/role/:role', userSalesDetail.getSalesTargetByRole);
+// router.get('/achivement/:user_id', userSalesDetail.calculateAchievement);
+
+// module.exports = router;
