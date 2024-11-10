ef984f6a8dfcb10e1a2fb9bb3e8a5c487cfff981
innogenx24 <git@innogenx.in>
2024-11-10 20:57:16 +0530

    product deletee

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -9,7 +9,7 @@ router.get('/', productController.getAllProducts);
 router.get('/:id', productController.getProductById);
 router.post('/', upload.single('image'), authMiddleware, isAdmin, productController.createProduct);
 router.put('/:id', upload.single('image'), productController.updateProduct);
-router.delete('/:id', productController.deleteProduct);
+router.delete('/:id', authMiddleware, isAdmin, productController.deleteProduct);
 
 module.exports = router;
 
