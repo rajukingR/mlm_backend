ec083e5bfc48ce061eb20c54a650ee0069a1c028
rajukingR <rajumulinti060@gmail.com>
2024-11-10 12:29:37 +0530

    UPDATE raju_server

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,14 +1,19 @@
 const multer = require('multer');
 const path = require('path');
 
+
+
+
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/'); // Store images in 'uploads/' directory
+    cb(null, 'uploads/');
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname)); // Unique filename
-  }
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
 });
+
 
 const fileFilter = (req, file, cb) => {
   if (file.mimetype.startsWith('image/')) {
@@ -21,7 +26,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
   storage: storage,
   fileFilter: fileFilter,
-  limits: { fileSize: 1024 * 1024 * 5 } // Limit file size to 5MB
+  limits: { fileSize: 1024 * 1024 * 5 },
 });
 
 module.exports = upload;
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,16 +1,14 @@
-'use strict';
-
 const express = require('express');
 const productController = require('../controllers/productController');
 const router = express.Router();
-const upload = require('../middlewares/multer'); // Make sure multer is set up correctly
+const upload = require('../middlewares/multer'); // Multer middleware for file uploads
 const { authMiddleware, isAdmin } = require('../middlewares/authMiddleware');
 
-// Define routes for CRUD operations
-router.post('/',upload.single('image'),authMiddleware,isAdmin, productController.createProduct);
+// Routes for CRUD operations
 router.get('/', productController.getAllProducts);
 router.get('/:id', productController.getProductById);
-router.put('/:id',upload.single('image'),authMiddleware,isAdmin, productController.updateProduct);
+router.post('/', upload.single('image'), authMiddleware, isAdmin, productController.createProduct);
+router.put('/:id', upload.single('image'), authMiddleware, isAdmin, productController.updateProduct);
 router.delete('/:id', productController.deleteProduct);
 
 module.exports = router;
