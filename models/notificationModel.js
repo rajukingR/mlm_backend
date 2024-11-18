4138140ac627b4fcdfcaca0d6136f55a8a4998e5
innogenx24 <git@innogenx.in>
2024-11-18 16:30:48 +0530

    notification completed

---
+++ models/notificationModel.js
@@ -0,0 +1,43 @@
+module.exports = (sequelize, DataTypes) => {
+    const Notification = sequelize.define(
+      'Notification',
+      {
+        id: {
+          type: DataTypes.INTEGER,
+          autoIncrement: true,
+          primaryKey: true,
+        },
+        user_id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+        },
+        message: {
+          type: DataTypes.TEXT,
+          allowNull: false,
+        },
+        is_read: {
+          type: DataTypes.BOOLEAN,
+          defaultValue: false,
+        },
+        created_at: {
+          type: DataTypes.DATE,
+          defaultValue: DataTypes.NOW, // Use DataTypes.NOW for default value
+        },
+      },
+      {
+        tableName: 'notifications', // Matches the name of your table
+        timestamps: false, // Disable Sequelize's default timestamps
+      }
+    );
+  
+    // Add associations if required
+    Notification.associate = (models) => {
+      Notification.belongsTo(models.User, {
+        foreignKey: 'user_id',
+        as: 'user',
+      });
+    };
+  
+    return Notification;
+  };
+  
