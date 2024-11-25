24e90675b4316d889ae04d1e2a575a5b28917515
innogenx24 <git@innogenx.in>
2024-11-25 18:50:56 +0530

    clear

--- routes/notification_new_orders/notificationNewOrdersRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getNotifications, markNotificationAsRead } = require('../../controllers/notification_new_order/notificationNewOrders');
-
-//******//
-router.get('/:user_id', getNotifications);
-router.patch('/:id', markNotificationAsRead);
-
-module.exports = router;
