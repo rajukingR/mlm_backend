20a6d74e37fd46bfb5048f457a7b9157ce895e4c
innogenx24 <git@innogenx.in>
2024-12-19 19:15:17 +0530

    mmm

--- routes/userSalesDetailRoutes.js
+++ routes/userSalesDetailRoutes.js
@@ -2,8 +2,10 @@
 const express = require('express');
 const router = express.Router();
 const userSalesDetail = require('../controllers/user_sales_detail/userSalesDetail');
+const { authMiddleware } = require('../middlewares/authMiddleware');
 
 router.get('/sales_achievement/:role/:user_id', userSalesDetail.getMonthlySalesDetails);
 router.get('/getLowHierarchySalesDetails/:user_id', userSalesDetail.getLowHierarchySalesDetails);
+router.get('/getLowHierarchySalesDetailsId/:user_id',userSalesDetail.getUserCurrentMonthSalesDetailsId);
 
 module.exports = router;
