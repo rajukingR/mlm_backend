cfbb77e8623c1f57fd24a63d7e8189c6eb5ba4cd
innogenx24 <git@innogenx.in>
2024-12-26 13:52:30 +0530

    home-page-apis

--- routes/overall_sales_route/overalSalesRoutes.js
+++ routes/overall_sales_route/overalSalesRoutes.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const { getOverallSalesCalculation } = require('../../controllers/overall_sales_calculation_controller/getOverallSalesCalculation');
+const { getOverallSalesCalculation,
+    getMostSellingProductPercentage,
+    salesOverTime,
+    getStockTargetDetails } = require('../../controllers/overall_sales_calculation_controller/getOverallSalesCalculation');
+const { authMiddleware } = require('../../middlewares/authMiddleware');
 
 
 // Route to get notifications
-router.get('/rolebased_sales', getOverallSalesCalculation);
+router.get('/rolebased_sales', authMiddleware, getOverallSalesCalculation);
+router.get('/mostl_selled_product', authMiddleware, getMostSellingProductPercentage);
+router.post('/sales_over_time', authMiddleware, salesOverTime);
+router.get('/stock_over_detail', authMiddleware, getStockTargetDetails);
+
 
 
 module.exports = router;
