baf116280c97c2f478396a4e07efa0b849d36552
innogenx24 <git@innogenx.in>
2025-01-24 19:19:13 +0530

    two sites setup

--- src/config/config.js
+++ src/config/config.js
@@ -1,10 +1,11 @@
 require('dotenv').config();  
 
+
 module.exports = {
   development: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    database: process.env.DB_NAME + '_dev',
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
   },
@@ -14,5 +15,6 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
+    logging: false,
   },
 };
