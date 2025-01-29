3985c87e2cb86ea0608769d3eb6334c6cab3cd82
rajukingR <rajumulinti060@gmail.com>
2025-01-29 10:12:26 +0530

    Updated

--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const categoryController = require('../controllers/category_controller/categoryController');
+const {  authMiddleware } = require('../middlewares/authMiddleware');
 
 router.post('/', categoryController.createCategory);
-router.get('/', categoryController.getAllCategories);
+router.get('/',authMiddleware, categoryController.getAllCategories);
 router.get('/:id', categoryController.getCategoryById);
 router.put('/:id', categoryController.updateCategory);
 router.delete('/:id', categoryController.deleteCategory);
