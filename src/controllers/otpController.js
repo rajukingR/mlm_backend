7af32fcc83fb303643e97649e542935d42fd29d8
rajukingR <rajumulinti060@gmail.com>
2025-02-06 10:50:45 +0530

    Updated

--- src/controllers/otpController.js
+++ src/controllers/otpController.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const twilio = require('twilio');
+require('dotenv').config();
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
